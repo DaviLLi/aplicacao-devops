f4b5872380653f33aa1d277a373b1a5b29c61c50
HiagoFarias <Hiagofaarias@gmail.com>
2026-04-23 21:09:20 -0300

    projeto eslint e workflow

---
+++ src/Garage.jsx
@@ -0,0 +1,11 @@
+import  Car from "./Car";
+
+const Garage = () => (
+    <>
+    <Car color="Prata" year="2024" />
+      <Car color="Vermelho" year="2012" />
+      <Car color="Branco" year="2010" />
+    </>
+)
+
+export default Garage;
